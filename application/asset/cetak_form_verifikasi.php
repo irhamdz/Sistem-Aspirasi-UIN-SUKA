30804e1c8af192e7ec94bbab6efea4065f750d6a
Irham Dzuhri <irhamdz@gmail.com>
2017-02-14 21:23:35 +0700

    first commit

---
+++ application/asset/cetak_form_verifikasi.php
@@ -0,0 +1,148 @@
+/*  
+Theme Name: simple MABA Print
+Theme URI: artharry.com
+Description: A clean simple MABA Print template.
+Author: artharry
+Author URI: artharry.com
+Version: 1.0
+*/
+
+body {
+	margin:0px;
+	font:11px Verdana, Arial, Helvetica, sans-serif;
+	line-height:15px;
+}
+
+hr {
+	background:#FFF;
+	border:#FFF;
+	size:1px;
+	page-break-after:always;
+}
+
+#print .pasfoto {
+	width:3.8cm;
+}
+#print {
+	margin-left:50px;
+}
+#print h3 {
+	font:bold 16px "Trebuchet MS", Arial, Helvetica, sans-serif;
+}
+
+#coverprint {
+	font-family:"Trebuchet MS", Arial, Helvetica, sans-serif;
+}
+#coverprint .big1 {
+	font-weight:bold;
+	font-size:28px;
+}
+#coverprint .big2 {
+	font-weight:bold;
+	font-size:20px;
+}
+#coverprint .big3 {
+	font-weight:bold;
+	font-size:15px;
+}
+#coverprint .sektorlokasiruang {
+	border-top:1px solid #000;
+	border-bottom:1px solid #000;
+	padding:10px 0 10px 0;
+}
+
+#headerprint {
+	padding:3px;
+	height:88px;
+	display:block;
+	font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+	border:1px solid #000;
+	margin-bottom:5px;
+}
+#headerprint .headerjudul {
+	font:bold 18px "Trebuchet MS", Arial, Helvetica, sans-serif;
+}
+#headerprint .headerlabel {
+	font:bold 12px "Trebuchet MS", Arial, Helvetica, sans-serif;
+}
+#headerprint .headercontent {
+	font:bold 12px "Trebuchet MS", Arial, Helvetica, sans-serif;
+}
+.tabelpesertaborder {
+	border:1px solid #000;
+	padding:3px;
+	font:13px "Trebuchet MS", Arial, Helvetica, sans-serif;
+	height:170px; 
+	margin-bottom:2px;
+}
+.tabelpesertaborder .masukdikit {
+	padding-left:20px;
+}
+.tabelpesertaborder .judul {
+	font:bold 13px "Trebuchet MS", Arial, Helvetica, sans-serif;
+}
+.tabelpesertaborder .bigger {
+	font:bold 14px "Trebuchet MS", Arial, Helvetica, sans-serif;
+}
+.tabelpesertabg {
+/*	background:url(images/pp2.png); */
+}
+
+.tabelttd {
+	background:#000;
+	font:10px "Trebuchet MS", Arial, Helvetica, sans-serif;
+	border-collapse: collapse;
+}
+.tabelttd td.ttd {
+/*	background:url(images/pp2.png); 
+	height:68px; */
+	height:55px;
+	text-align:center;
+	vertical-align:bottom;
+}
+.tabelttd td {
+/*	background:url(images/pp2.png);*/
+	background:#FFF;
+	border: 1px solid;
+	text-align:center;
+	vertical-align:middle;
+}
+.tabelttd th {
+/*	background:url(images/pp2.png);*/
+	background:#FFF;
+	text-align:center;
+	vertical-align:middle;
+}
+
+#infohal {
+	font:12px "Trebuchet MS", Arial, Helvetica, sans-serif;
+	text-align:right;
+}
+
+.tabstat{
+	margin-left: 0px;
+}
+.tabstat table#jadw{
+	border-collapse: collapse;
+}
+.tabstat tr#judd{
+	font-weight: bold;
+	background: #888;
+	color: #eee;
+	height: 25px;
+}
+.tabstat td{
+	border: 1px solid #000;
+	vertical-align: text-top;
+	height: 20px;
+}
+.tabstat tr#iss:hover{
+	background: #ddd;
+}
+.tabstat a{
+	font-weight: bold;
+	text-decoration: underline;
+}
+.tabstat a:hover{
+	text-decoration: none;
+}
